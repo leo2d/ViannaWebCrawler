f50857abdbbcc9db07ebf77d2bfaa5faa8253b0c
Leonardo Dias <leonardo.dias@guiando.com.br>
2018-05-21 18:57:16 -0300

    Criacao do projeto, classe modelo e adicao das referencias necessarias.

---
+++ ViannaWebCrawler/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+using System.Net.Http;
+
+namespace ViannaWebCrawler
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StartCrawlerAsync();
+
+
+            Console.ReadKey();
+        }
+
+        private static async Task StartCrawlerAsync()
+        {
+            var url = "http://aluno.viannajr.edu.br/";
+            var httpClient = new HttpClient();
+            var html = await httpClient.GetStringAsync(url);
+            var htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(html);
+            //    var div =
+            //        htmlDocument.DocumentNode.Descendants("div")
+            //            .Where(x -> x.GetatributeValue("class", "").Equals("form-control")).ToList();
+        }
+    }
+
+}
